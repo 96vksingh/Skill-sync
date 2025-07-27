72034b7e97bc23cb061f8f7e6c4b1d70b2eaf741
Vishal Kumar Singh <vishal.k@zerozilla.com>
2025-07-28 04:23:42 +0530

    updated new backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://skill-sync-backend-d6v3.onrender.com/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://skill-sync-backend-hggm.onrender.com/api';
 
 
 async function preparednessScore(level, years) {
